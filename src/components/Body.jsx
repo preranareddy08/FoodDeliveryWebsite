b470346712cac261536757999d90c58b3a2f84a1
Prerana Reddy <preranareddy08@gmail.com>
2026-02-03 12:51:33 +0530

    added useEffect hook

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,33 +1,59 @@
-import React from "react";
-import { useState } from "react";
+import React, { useEffect, useState } from "react";
 import RestaurentCard from "./RestaurentCard";
-import { resObj } from "../utils/mockData";
 
 const Body = () => {
-    //declaring a state variable
-    const [ListofRestaurent, setListofRestaurent] = useState(resObj);
+  const [ListofRestaurent, setListofRestaurent] = useState([]);
 
-    return(
-        <div className="body">
-            <div className="filter">
-                <button className="filter-btn"
-                        onClick = {()=>{
-                        const filteredList = ListofRestaurent.filter(
-                            (res)=>res.card.card.info.avgRating>4.5);
-                        setListofRestaurent(filteredList);
-                        }}
-                    >Top Rated Restaurent</button>
-            </div>
-            <div className="res-container">
-            {
-                ListofRestaurent.map((restaurent) => (
-                    <RestaurentCard 
-                    key={restaurent.card.card.info.id}
-                      resData={restaurent.card.card.info}/>
-                ))
-            }
-            </div>
-        </div>
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=17.325403&lng=78.6257302&collection=83639&tags=layout_CCS_Biryani&sortBy=&filters=&type=rcv2&offset=0&page_type=null"
     );
+
+    const json = await data.json();
+    console.log(json);
+
+    const restaurants =
+      json?.data?.cards
+        ?.filter(
+          (card) =>
+            card?.card?.card?.["@type"] ===
+            "type.googleapis.com/swiggy.presentation.food.v2.Restaurant"
+        )
+        ?.map((card) => card.card.card.info);
+
+    setListofRestaurent(restaurants);
+  };
+
+  return (
+    <div className="body">
+      <div className="filter">
+        <button
+          className="filter-btn"
+          onClick={() => {
+            const filteredList = ListofRestaurent.filter(
+              (res) => res.avgRating > 4
+            );
+            setListofRestaurent(filteredList);
+          }}
+        >
+          Top Rated Restaurant
+        </button>
+      </div>
+
+      <div className="res-container">
+        {ListofRestaurent.map((restaurant) => (
+          <RestaurentCard
+            key={restaurant.id}
+            resData={restaurant}
+          />
+        ))}
+      </div>
+    </div>
+  );
 };
+
 export default Body;
